8370c37e5d958add96a536a897fa40685f9008d3
Kat Leight <leightkt@gmail.com>
2021-03-16 20:05:20 -0600

    wrote routes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+
+    def create
+        @comment = Comment.create(params)
+        render json: { "comment: #{@comment}"}
+    end
+    
+    def destroy
+        @comment = Comment.find(params[:id])
+        render json: { "comment ##{@comment.id} deleted"}
+    end
 end
--- app/controllers/cults_controller.rb
+++ app/controllers/cults_controller.rb
@@ -1,2 +1,14 @@
 class CultsController < ApplicationController
+
+    def index
+        @cults = Cult.all
+        render json: @cults, include: :comments
+    end
+
+    def update
+        @cult.update(params)
+        render json: @cult, include: :comments
+    end
+
+    
 end
